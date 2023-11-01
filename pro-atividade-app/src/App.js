00f2ada96c1a0c96885bd0e66ffa0c80a05bf01b
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2023-11-01 17:47:02 -0300

    Utilizando useState - 16

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ListGroup from "react-bootstrap/ListGroup";
+
+let initialState = [
+  {
+    id: 1,
+    descricao: "Primeira Atividade",
+  },
+  {
+    id: 2,
+    descricao: "Segunda Atividade",
+  },
+];
 
 function App() {
+  const [atividades, setAtividades] = useState(initialState);
+
+  function addAtividade(e) {
+    e.preventDefault();
+    const atividade = {
+      id: document.getElementById("id").value,
+      descricao: document.getElementById("descricao").value,
+    };
+    setAtividades([...atividades, { ...atividade }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input id="id" type="text" placeholder="id"></input>
+        <input id="descricao" type="text" placeholder="descrição"></input>
+        <button onClick={addAtividade}>+ Atividade</button>
+      </form>
+      <div className="mt-3">
+        <ListGroup>
+          {atividades.map((ativ) => (
+            <ListGroup.Item key={ativ.id}>
+              {ativ.id} - {ativ.descricao}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    </>
   );
 }
 
