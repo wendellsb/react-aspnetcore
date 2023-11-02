ce906c47821934d16cc3bdfac0ef497e4a120a84
Wendell Santos <88899145+wendellsb@users.noreply.github.com>
2023-11-02 17:02:25 -0300

    Evoluindo Form

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import ListGroup from "react-bootstrap/ListGroup";
 
 let initialState = [
   {
@@ -27,19 +26,66 @@ function App() {
 
   return (
     <>
-      <form>
-        <input id="id" type="text" placeholder="id"></input>
-        <input id="descricao" type="text" placeholder="descrição"></input>
-        <button onClick={addAtividade}>+ Atividade</button>
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label className="form-label">Id</label>
+          <input
+            id="id"
+            type="text"
+            className="form-control"
+            placeholder="id"
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Descrição</label>
+          <input
+            id="descricao"
+            type="text"
+            className="form-control"
+            placeholder="descrição"
+          />
+        </div>
+        <hr />
+        <div className="col-12">
+          <button className="btn btn-outline-secondary" onClick={addAtividade}>
+            + Atividade
+          </button>
+        </div>
       </form>
       <div className="mt-3">
-        <ListGroup>
-          {atividades.map((ativ) => (
-            <ListGroup.Item key={ativ.id}>
-              {ativ.id} - {ativ.descricao}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
+        {atividades.map((ativ) => (
+          <div key={ativ.id} className="card mb-2 shadow">
+            <div className="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 className="card-title">
+                  <span className="badge text-bg-secondary me-2">
+                    {ativ.id}
+                  </span>
+                  - título
+                </h5>
+                <h6>
+                  Prioridade:
+                  <span className="ms-1 text-black">
+                    <i className="me-1 fa-regular fa-face-frown"></i>Alta
+                  </span>
+                </h6>
+              </div>
+
+              <p className="card-text">{ativ.descricao}</p>
+
+              <div className="d-flex justify-content-end pt-2 m-0 border-top">
+                <button className="btn btn-outline-primary me-2 btn-sm">
+                  <i className="fa-solid fa-pen me-2"></i>
+                  Editar
+                </button>
+                <button className="btn btn-outline-danger btn-sm">
+                  <i className="fa-solid fa-trash me-2"></i>
+                  Deletar
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
